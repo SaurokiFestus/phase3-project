c5d73d72fb4ee8cf05acfcc24303e28fbf6899f0
SaurokiFestus <sauroki.festus@student.moringaschool.com>
2022-09-06 13:25:01 +0300

    milestone: fetch and display data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-// import Home from "./pages/Home";
+import userEvent from "@testing-library/user-event";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
+import {useEffect, useState, useContext} from "react";
+
 function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => res.json())
+    .then(data => setUsers(data))
+    console.log(setUsers)
+  },[])
   return (
     <div className="App">
-      {/* <Home/> */}
-      <Login/>
+      {users.map((user) =>(
+        <Home name ={user.name} username ={user.username} email ={user.email}/>
+      ))}
+      
+      {/* <Login/> */}
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,45 @@
 import React from 'react'
-import { Typography, Button } from '@mui/material'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import {useEffect, useState, useContext} from "react"
+import { ClassNames } from '@emotion/react';
 
-function Home() {
+
+function Home({name, username, email}) {
+
+  
   return (
-    <div>
-        <Typography 
-        variant='h6'
-        color="textSecondary"
-        component="h2"
-        gutterBottom
-        >
-            Homepage
-        </Typography>
-        <Button
-        variant="primary"
-        >
-            Primary
-        </Button>
-
-    </div>
-  )
+    <>
+    
+    <Card sx={{ 
+      maxWidth: 345,
+      marginBottom: 5,
+      }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://image.shutterstock.com/image-photo/billboard-canvas-mockup-city-background-600w-539175535.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Name:{name}
+          </Typography>
+          <Typography gutterBottom variant="h6" component="div">
+            Location: {username}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Description: {email}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    </>
+  );
 }
 
 export default Home
